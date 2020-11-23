fe126a41415d05fb7eb3ad4ec60417a58af46c4c
HitConfirm <74881887+HitConfirm@users.noreply.github.com>
2020-11-23 00:49:33 -0800

    Add files via upload
    This asset folder update contains a script for continual forward movement with left-right player input, a player moving camera, and a score that updates with movement along the Z axis.

---
+++ BrackeysTutorial/Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+ // FixedUpdate used in Unity for physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+       
+        if (Input.GetKey ("d")) // Player pressing "d" key
+        {
+            // Add a Force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) // Player pressing "a" key
+        {
+            // Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }  
+    }
+}
